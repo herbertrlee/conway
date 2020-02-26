d0ac8868e768f9c2ea4bd2f4da8b19fc04b74d3f
Herbert Lee <herbert.lee@striiv.com>
2020-02-25 23:31:40 -0800

    Added turn on and turn off methods.

--- conway/game.py
+++ conway/game.py
@@ -9,3 +9,12 @@ class Game:
     @property
     def board(self) -> np.array:
         return self._board
+
+    def turn_on(self, row: int, col: int):
+        self._set_cell(row, col, True)
+
+    def _set_cell(self, row: int, col: int, value: bool):
+        self.board[row][col] = value
+
+    def turn_off(self, row: int, col: int):
+        self._set_cell(row, col, False)
--- tests/test_conway/test_game.py
+++ tests/test_conway/test_game.py
@@ -7,9 +7,41 @@ from conway.game import Game
 
 class GameTests(TestCase):
 
-    def test_initial_board(self):
+    def setUp(self) -> None:
         self.game = Game(3, 3)
 
+    def test_initial_board(self):
         expected_board = np.zeros([3, 3], dtype=np.bool)
 
+        np.testing.assert_array_equal(expected_board, self.game.board)
+
+    def test_turn_on_cell(self):
+        self.game.turn_on(1, 0)
+
+        expected_board = np.array(
+            [
+                [False, False, False],
+                [True, False, False],
+                [False, False, False]
+            ],
+            dtype=np.bool
+        )
+
+        np.testing.assert_array_equal(expected_board, self.game.board)
+
+    def test_turn_off_cell(self):
+        for i in range(3):
+            for j in range(3):
+                self.game.turn_on(i, j)
+
+        self.game.turn_off(0, 1)
+
+        expected_board = np.array(
+            [
+                [True, False, True],
+                [True, True, True],
+                [True, True, True]
+            ],
+            dtype=np.bool
+        )
         np.testing.assert_array_equal(expected_board, self.game.board)
