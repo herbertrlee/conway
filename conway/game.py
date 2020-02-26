d7db8e42e5cdca564206d2944c59b3a7d0aa101b
Herbert Lee <herbert.lee@striiv.com>
2020-02-26 00:17:02 -0800

    Implemented rules for game.

--- conway/game.py
+++ conway/game.py
@@ -3,12 +3,20 @@ import numpy as np
 
 class Game:
 
-    def __init__(self, width: int, height: int):
-        self._board = np.zeros([width, height], dtype=np.bool)
+    def __init__(self, height: int, width: int):
+        self._height = height
+        self._width = width
+        self._board = np.zeros([height, width], dtype=np.bool)
 
     @property
     def board(self) -> np.array:
         return self._board
+
+    def is_alive(self, row: int, col: int) -> bool:
+        try:
+            return self._board[row][col]
+        except IndexError:
+            return False
 
     def turn_on(self, row: int, col: int):
         self._set_cell(row, col, True)
@@ -18,3 +26,30 @@ class Game:
 
     def turn_off(self, row: int, col: int):
         self._set_cell(row, col, False)
+
+    def step(self):
+        next_board = np.zeros(self._board.shape)
+
+        for i in range(self._height):
+            for j in range(self._width):
+                next_board[i][j] = self._get_next_step_cell(i, j)
+
+        self._board = next_board
+
+    def _get_next_step_cell(self, i: int, j: int) -> bool:
+        neighbor_count = self._get_neighbor_count(i, j)
+
+        if self.is_alive(i, j) and neighbor_count in (2, 3):
+            return True
+        elif not self.is_alive(i, j) and neighbor_count == 3:
+            return True
+
+        return False
+
+    def _get_neighbor_count(self, i: int, j: int) -> int:
+        neighbor_indices = [
+            (i - 1, j - 1), (i, j - 1), (i + 1, j - 1),
+            (i - 1, j), (i + 1, j),
+            (i - 1, j + 1), (i, j + 1), (i + 1, j + 1)
+        ]
+        return sum(1 for row, col in neighbor_indices if self.is_alive(row, col))
--- tests/test_conway/test_game.py
+++ tests/test_conway/test_game.py
@@ -8,40 +8,82 @@ from conway.game import Game
 class GameTests(TestCase):
 
     def setUp(self) -> None:
-        self.game = Game(3, 3)
+        self.game = Game(4, 3)
 
     def test_initial_board(self):
-        expected_board = np.zeros([3, 3], dtype=np.bool)
+        expected_board = np.zeros([4, 3], dtype=np.bool)
 
         np.testing.assert_array_equal(expected_board, self.game.board)
 
     def test_turn_on_cell(self):
         self.game.turn_on(1, 0)
 
-        expected_board = np.array(
-            [
-                [False, False, False],
-                [True, False, False],
-                [False, False, False]
-            ],
-            dtype=np.bool
-        )
-
-        np.testing.assert_array_equal(expected_board, self.game.board)
+        self.assertTrue(self.game.is_alive(1, 0))
 
     def test_turn_off_cell(self):
-        for i in range(3):
-            for j in range(3):
-                self.game.turn_on(i, j)
-
+        self.game.turn_on(0, 1)
         self.game.turn_off(0, 1)
 
-        expected_board = np.array(
-            [
-                [True, False, True],
-                [True, True, True],
-                [True, True, True]
-            ],
-            dtype=np.bool
-        )
+        self.assertFalse(self.game.is_alive(0, 1))
+
+    def test_step_empty(self):
+        self.game.step()
+
+        expected_board = np.zeros([4, 3], dtype=np.bool)
+
         np.testing.assert_array_equal(expected_board, self.game.board)
+
+    def test_step_underpopulated_0(self):
+        self.game.turn_on(1, 1)
+
+        self.game.step()
+
+        self.assertFalse(self.game.is_alive(1, 1))
+
+    def test_step_underpopulated_1(self):
+        self.game.turn_on(0, 1)
+        self.game.turn_on(1, 1)
+
+        self.game.step()
+
+        self.assertFalse(self.game.is_alive(0, 1))
+        self.assertFalse(self.game.is_alive(1, 1))
+
+    def test_step_keep_alive_2(self):
+        self.game.turn_on(0, 1)
+        self.game.turn_on(1, 1)
+        self.game.turn_on(2, 1)
+
+        self.game.step()
+
+        self.assertTrue(self.game.is_alive(1, 1))
+
+    def test_step_keep_alive_3(self):
+        self.game.turn_on(0, 1)
+        self.game.turn_on(1, 1)
+        self.game.turn_on(2, 1)
+        self.game.turn_on(1, 2)
+
+        self.game.step()
+
+        self.assertTrue(self.game.is_alive(1, 1))
+
+    def test_step_overpopulation(self):
+        self.game.turn_on(0, 0)
+        self.game.turn_on(0, 1)
+        self.game.turn_on(1, 1)
+        self.game.turn_on(2, 1)
+        self.game.turn_on(1, 2)
+
+        self.game.step()
+
+        self.assertFalse(self.game.is_alive(1, 1))
+
+    def test_step_procreation(self):
+        self.game.turn_on(0, 0)
+        self.game.turn_on(0, 1)
+        self.game.turn_on(2, 1)
+
+        self.game.step()
+
+        self.assertTrue(self.game.is_alive(1, 1))
